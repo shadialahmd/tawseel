e305f84523bf350ac443646d2bc26095c6d0b0df
shadialahmd <shadi.alahmd@gmail.com>
2021-02-07 15:18:12 +0400

    fpdf

--- Include/operation.php
+++ Include/operation.php
@@ -1,5 +1,8 @@
 <?php
 
+
+include_once "Lib/fpdf.php";
+
 class Operation{
 
 
@@ -16,11 +19,41 @@ class Operation{
 
          $sql="INSERT INTO orders (Recipient_Name,Phone,Address,Amount,Status) VALUES('$data[recipient_name]','$data[phone]','$data[address]',$data[amount],'$data[status]')";
          $result=mysqli_query($this->conn,$sql);
-
-
-        print_r($data);
+         $this->createInvoice();
 
     }
-}
+
+    public function createInvoice(){
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','B',16);
+        $pdf->Cell(40,10,'Hello World!');
+        $pdf->Output('D','ddd.pdf');
+
+    }
+
+
+    
+        
+
+
+        
+     
+
+       
+     
+
+
+
+
+
+  
+       
+        
+
+
+
+    }
+
 
 ?>
--- home.php
+++ home.php
@@ -6,33 +6,61 @@ if (!isset($_SESSION['id']) ||(trim ($_SESSION['id']) == '')){
 	header('location:index.php');
 }
 
-include_once('code.php');
+include_once('Include/operation.php');
 
 include_once('Include/dbconfig.php');
 
 $batabase=new Database();
 $db=$batabase->getConnection();
 
+$op=new Operation($db);
 
+if(isset($_POST["submit"])){
+$op->addOrder($_POST);
+
+}
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Login using OOP Approach</title>
+	<title>تسجيل طلب توصيل جديد</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
-	<h1 class="page-header text-center">PHP Login using OOP Approach</h1>
+	<h1 class="page-header text-center">تسجيل طلب توصيل جديد</h1>
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
-			<h2>Welcome to Homepage </h2>
-			<h4>User Info: </h4>
-			<p>Name: <?php echo $data['First Name']; ?></p>
-			<p>Username: <?php echo $data['Last Name']; ?></p>
-			<p>Password: <?php echo $data['Email']; ?></p>
-			<a href="logout.php" class="btn btn-danger"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+		<form id="submitForm" method="post" action="">
+      
+	  <div class="form-group">
+		  <label for="name">إسم المستلم</label>
+		  <input type="text" class="form-control" name="recipient_name" placeholder="أدخل الإسم التجاري" >
+		</div>
+	  <div class="form-group">
+		  <label for="name">العنوان</label>
+		  <input type="text" class="form-control" name="phone" placeholder="الإسم الأول" >
+		</div>
+		<div class="form-group">  
+		  <label for="mobile">رقم العاتف</label>
+		  <input type="text" class="form-control" name="address" placeholder="إسم العائلة" >
+		</div>
+		<div class="form-group">  
+		  <label for="nmail">المبلغ</label>
+		  <input type="text" class="form-control" name="amount" placeholder="ادخل البريد الإلكتروني" >
+		</div>
+		<div class="form-group">  
+		  <label for="nmail">الحالة</label>
+		  <input type="text" class="form-control" name="status" placeholder="كلمة المرور" >
+		</div>
+
+	
+		<div class="form-group">
+		  <p>Already have account ?<a href="login.php"> Login</a></p>
+		  <button type="submit"  name="submit" class="btn btn-primary">Sign Up</button>
+		</div>
+	  </form>
 		</div>
 	</div>
 </div>
